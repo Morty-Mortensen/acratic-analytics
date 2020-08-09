acc859bf45e68384a86bd6a7b12da02b61bfef6f
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2020-08-08 23:20:39 -0600

    added nav-bar component, still need to style it and such.

--- front-end/autocratic-analytics/src/app/app.module.ts
+++ front-end/autocratic-analytics/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from './navigation/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
